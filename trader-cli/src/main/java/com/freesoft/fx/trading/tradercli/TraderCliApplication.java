04823d6fbd94bb73984f5871a6c2eeea732817fb
dinabogdan <bogdan.dina03@gmail.com>
2020-09-22 11:00:39 +0300

    add trader-cli

---
+++ trader-cli/src/main/java/com/freesoft/fx/trading/tradercli/TraderCliApplication.java
@@ -0,0 +1,13 @@
+package com.freesoft.fx.trading.tradercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderCliApplication.class, args);
+	}
+
+}
---
+++ trader-cli/src/test/java/com/freesoft/fx/trading/tradercli/TraderCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freesoft.fx.trading.tradercli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
